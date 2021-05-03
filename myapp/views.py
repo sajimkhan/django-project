46de0a59fdf34a5f76eaf86bf93b5f04d9abc1be
sajimkhan <shahadate15-2691@diu.edu.bd>
2021-05-03 22:37:04 +0600

    first uploading

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from .models import Profile
+
+def homepage(request):
+
+    profile = Profile.objects.all()
+
+    context = {
+        'profile': profile
+    }
+
+    return render(request, 'index.html', context)
